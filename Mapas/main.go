38afaa0d6f72b0cc6dd119dac41b7753e6ba29c5
Paula Santos <paulac.usp@gmail.com>
2018-02-28 13:26:30 -0300

    mapas

---
+++ Mapas/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/paulinhacnn/Mapas/model"
+)
+
+var cache map[string]model.Gene
+
+func main() {
+
+	cache = make(map[string]model.Gene, 0)
+
+	dna := model.Gene{}
+	dna.Nome = "Cromossomo 21"
+	dna.X = 21
+	dna.Y = 22
+	dna.SetSequencia("ACTG")
+	objJSON, _ := json.Marshal(dna)
+
+	fmt.Printf("Gene é Json: %+v\r\n", string(objJSON))
+	fmt.Printf("Gene é: %+v\r\n", dna)
+	fmt.Printf("O valor da casa é: %d\r\n", dna.GetSequencia())
+
+	cache["Cromossomo 21"] = dna
+
+	fmt.Println("Encontrou o cromosso 21?")
+
+	dna, achou := cache["Cromossomo 21"]
+	if achou {
+		fmt.Println("Sim, encontrei: %+v\r\n", dna)
+	}
+
+}
